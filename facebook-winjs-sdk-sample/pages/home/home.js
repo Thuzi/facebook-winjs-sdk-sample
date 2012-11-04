1be79b5f6db3d6c907d3e8ecce0425c8fe3c9169
Prabir Shrestha <mail@prabir.me>
2012-11-04 08:47:37 -0500

    initial commit

---
+++ facebook-winjs-sdk-sample/pages/home/home.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            WinJS.Utilities.query('#logout').listen('click', logoutClicked, false);
+
+            loadData();
+        }
+    });
+
+    function logoutClicked(e) {
+        e.preventDefault();
+
+        localStorage.removeItem('fb_access_token');
+        FB.setAccessToken(null);
+
+        WinJS.Navigation.navigate('/pages/login/login.html');
+    }
+
+    function loadData() {
+        FB.api('me', function(res) {
+            if(!res || res.error) {
+                console.log(!res ? 'error occurred' : res.error);
+                return;
+            }
+
+            $('#hi').html('Hi ' + res.name + '!');
+        });
+    }
+    
+})();
